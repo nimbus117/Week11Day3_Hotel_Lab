2f1ea167eca64a82c00f780f7c2a3b8898217284
Sharon Kynaston <desmogal@gmail.com>
2018-10-10 14:08:01 +0100

    created booking class - tested

---
+++ src/main/java/Booking.java
@@ -0,0 +1,22 @@
+
+
+public class Booking {
+
+
+    Bedroom bedroom;
+    int numNights;
+
+    public Booking(Bedroom bedroom, int numNights){
+        this.bedroom = bedroom;
+        this.numNights = numNights;
+    }
+
+    public Bedroom getBedroom() {
+        return bedroom;
+    }
+
+
+    public int getNumNights() {
+        return numNights;
+    }
+}
---
+++ src/test/java/BookingTest.java
@@ -0,0 +1,33 @@
+import com.sun.jndi.ldap.Ber;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
+public class BookingTest {
+
+    Bedroom bedroom;
+
+
+    @Before
+    public void before() {
+        bedroom = new Bedroom(2, "single");
+    }
+
+    @Test
+    public void hasBedroom(){
+        Booking booking = new Booking(bedroom, 2);
+        assertEquals(bedroom, booking.getBedroom());
+    }
+
+    @Test
+    public void hasNumNights(){
+        Booking booking = new Booking(bedroom, 2);
+        assertEquals(2, booking.getNumNights());
+    }
+
+
+
+}
