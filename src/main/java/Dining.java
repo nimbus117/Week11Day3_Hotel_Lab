91a517748691359c6b7217d5fb05b0fd110bb9fa
Sharon Kynaston <desmogal@gmail.com>
2018-10-10 11:32:21 +0100

    added/passed dining tests

--- src/main/java/Dining.java
+++ src/main/java/Dining.java
@@ -1,2 +1,27 @@
+import java.util.ArrayList;
+
 public class Dining {
+    String roomName;
+    int capacity;
+    ArrayList<Guest> guests;
+
+    public Dining (String roomName, int capacity ){
+
+        this.roomName = roomName;
+        this.capacity = capacity;
+        guests = new ArrayList<>();
+
+    }
+
+    public String getRoomName() {
+        return roomName;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public int getRoomGuestCount() {
+        return guests.size();
+    }
 }
--- src/test/java/DiningTest.java
+++ src/test/java/DiningTest.java
@@ -1,2 +1,31 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class DiningTest {
+
+    private Dining room1;
+
+    @Before
+    public void before() {
+        room1 = new Dining("Main", 20);
+    }
+
+    @Test
+    public void hasRoomName(){
+        assertEquals("Main", room1.getRoomName());
+    }
+
+    @Test
+    public void roomHasCapacity(){
+        assertEquals(20, room1.getCapacity());
+    }
+
+    @Test
+    public void hasNoGuests(){
+
+        assertEquals(0, room1.getRoomGuestCount());
+    }
+
 }
