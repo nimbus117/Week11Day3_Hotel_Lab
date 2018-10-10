b4d1c15431a9d18a20c173bbb2bc7ae9c33d9567
Sharon Kynaston <desmogal@gmail.com>
2018-10-10 11:34:05 +0100

    added/passed conference tests

--- src/test/java/ConferenceTest.java
+++ src/test/java/ConferenceTest.java
@@ -1,2 +1,31 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class ConferenceTest {
+
+    private Conference room1;
+
+    @Before
+    public void before() {
+        room1 = new Conference("Main", 20);
+    }
+
+    @Test
+    public void hasRoomName(){
+        assertEquals("Main", room1.getRoomName());
+    }
+
+    @Test
+    public void roomHasCapacity(){
+        assertEquals(20, room1.getCapacity());
+    }
+
+    @Test
+    public void hasNoGuests(){
+
+        assertEquals(0, room1.getRoomGuestCount());
+    }
+
 }
